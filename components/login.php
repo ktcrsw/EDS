b45a8d461db3ec14d95b10cea9f66471d406fc2f
Kittichai Raksawong <kittichdev@gmail.com>
2023-05-21 19:31:34 +0700

    Urs Change

--- components/login.php
+++ components/login.php
@@ -1,6 +1,6 @@
 <?php include("../assets/header.php");?>
   <main>
-    <form action="../../backend/auth/signin.php" method="post">
+    <form action="../Backend/auth/signin.php" method="post">
         <input class="m-5 border-solid border-2 rounded border-black" type="text" name="idcard" id="idcard" minlength="13" placeholder="รหัสบัตรประชาชน" required>
         <input class="m-5 border-solid border-2 rounded border-black" type="text" name="pwd" id="pwd" placeholder="รหัสผ่าน" required>
         <input type="submit" value="submit">
---
+++ components/users/index.php
@@ -0,0 +1,2 @@
+<?php include"assets/header.php"; ?>
+<a name="" id="" class="btn btn-primary" href="../Backend/auth/signout.php" role="button">LogOut</a>
