f38dc1f288cb7b86c271699f9bf8950e9d246014
Kittichai Raksawong <kittichdev@gmail.com>
2023-05-24 13:33:39 +0700

    index

--- components/teacher/index.php
+++ components/teacher/index.php
@@ -6,8 +6,6 @@
       include "../assets/teacher_nav.php";
 ?>
 
-  <a name="" id="" class="m-5 mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" href="subject.php" role="button">ลงเวลาเรียน</a>
-
 
 <div class="p-4 sm:ml-64">
    <div class="p-4 rounded-lg dark:border-gray-700 mt-14">
