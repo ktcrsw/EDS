9ac19d9f5289cea02fb09f4d0829f4f25638c61f
Bongkotphetr <arthamyai222@gmail.com>
2023-06-03 03:14:14 +0700

    'hey'

--- pagkage/tailwind.config.js
+++ pagkage/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./components/teacher/index.php"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow:{
+        'login-sha':'0px 3px 15px rgba(0, 0, 0, 0.25)',
+      },
+    },
   },
   plugins: [require('prettier-plugin-tailwindcss')],
 }
