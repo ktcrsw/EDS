71150e7227bd5e6f231f6161f6fddb766204fc81
Kittichai Raksawong <kittichdev@gmail.com>
2023-06-07 13:18:20 +0700

    Nav

--- components/assets/teacher_nav.php
+++ components/assets/teacher_nav.php
@@ -31,7 +31,7 @@
    <div class="h-full px-3 pb-4 overflow-y-auto bg-white duration-500 overflow-hidden w-14 hover:w-64 dark:bg-gray-800">
       <ul class="space-y-2 font-medium">
       <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+            <a href="../teacher/index.php" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
             <svg fill="none" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
   <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path>
 </svg>
