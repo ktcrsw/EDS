9ab2facf74feba7e5f465693c3b9e304711249f8
Kittichai Raksawong <kittichdev@gmail.com>
2023-06-20 13:59:19 +0700

    Yeah

---
+++ components/teacher/edit_profileData.php
@@ -0,0 +1,74 @@
+<?php session_start(); ?>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="sweetalert2.all.min.js"></script>
+<?php include "../../Backend/db/connect.db.php";
+include "../assets/header.php";
+include "../assets/teacher_nav.php";
+
+
+$sql = "SELECT * FROM enrolltbl";
+$query = $db->query($sql);
+
+$sql = "SELECT * FROM users";
+$query = $db->query($sql);
+
+$subject = "SELECT * FROM subjecttbl";
+$data = $db->query($subject);
+?>
+
+
+<section class="m-2">
+     
+<div class="mb-4 border-b border-gray-200 dark:border-gray-700">
+    <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="myTab" data-tabs-toggle="#myTabContent" role="tablist">
+        <li class="mr-2" role="presentation">
+            <button class="inline-block p-4 border-b-2 rounded-t-lg" id="profile-tab" data-tabs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">จัดการตารางเรียน</button>
+        </li>
+        <li class="mr-2" role="presentation">
+            <button class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab" aria-controls="dashboard" aria-selected="false">เช็คชื่อนักศึกษาเข้าร่วมกิจกรรมหน้าเสาธง</button>
+        </li>
+        <li class="mr-2" role="presentation">
+            <button class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="settings-tab" data-tabs-target="#settings" type="button" role="tab" aria-controls="settings" aria-selected="false">เช็คชื่อนักศึกษาเข้าเรียนรายวิชา</button>
+        </li>
+        <li class="mr-2" role="presentation">
+            <button class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="settings-tab" data-tabs-target="#settings" type="button" role="tab" aria-controls="settings" aria-selected="false">พิมพ์รายงานเอกสาร</button>
+        </li>
+    </ul>
+</div>
+<div id="myTabContent">
+    <div class="hidden" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+   
+        <div>
+        <div class="px-4 md:px-10 ">
+            <div class="flex items-center justify-between">
+                <div class="">
+
+                </div>
+            </div>
+            <p tabindex="0" class="focus:outline-none text-base sm:text-lg md:text-xl lg:text-2xl font-bold leading-normal text-gray-800">จัดการตารางเรียน</p>
+        </div>
+        
+        
+        </div>
+
+
+        
+    </div>
+
+
+
+
+
+
+    <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="dashboard" role="tabpanel" aria-labelledby="dashboard-tab">
+        <?php include ""; ?>
+    </div>
+    <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="settings" role="tabpanel" aria-labelledby="settings-tab">
+    <?php include ""; ?>
+    </div>
+    <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="contacts" role="tabpanel" aria-labelledby="contacts-tab">
+    <?php include ""; ?>
+    </div>
+</div>
+
+</section>
