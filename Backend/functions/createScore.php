00af002c17dcd9619dbbefbe6c01492d86c2a42c
dadaKitt <kittichdev@gmail.com>
2023-07-31 15:04:31 +0700

    Score

--- Backend/functions/createScore.php
+++ Backend/functions/createScore.php
@@ -3,20 +3,23 @@
     session_start();
     include '../db/connect.db.php';
     
+    $mindScore = $_POST['mind'];
     $theoryScore = $_POST['theory'];
     $carryScore = $_POST['carry'];
     $startdate = $_POST['startdate'];
     $enddate = $_POST['enddate'];
     $teacherID = $_SESSION['UserID'];
 
+    echo $mindScore;
     echo $theoryScore;
     echo $carryScore;
     echo $startdate;
     echo $enddate;
 
 
-    $saveScore = "INSERT INTO createscore(createScoreID, createScoreTeacherID, createScoreMind, createScoreTheory, createScoreCarry, createScoreFinal, createScoreStartDate, createScoreEndDate) VALUES ('', '$teacherID', '0', '$theoryScore', '$carryScore', '0', '$startdate', '$enddate');";
+    $saveScore = "INSERT INTO createscore(createScoreID, createScoreTeacherID, createScoreMind, createScoreTheory, createScoreCarry, createScoreFinal, createScoreStartDate, createScoreEndDate) VALUES ('', '$teacherID', $mindScore', '$theoryScore', '$carryScore', '', '$startdate', '$enddate');";
     $queryScore = $db->query($saveScore);
+
 
 
 ?>
